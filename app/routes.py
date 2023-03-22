bc724e8449275436d45d41b2250d8c8aa00b3754
Lukasz Gorka <lukaszgorka@hotmail.com>
2023-03-22 12:44:29 +0100

    add route update_book

--- app/routes.py
+++ app/routes.py
@@ -53,3 +53,58 @@ def delete_book():
     else:
         msg = 'Book not found'
     return render_template('delete.html', msg = msg)
+
+@app.route('/find_book', methods = ['GET'])
+def find():
+    return render_template("update.html")
+
+@app.route('/find_book', methods = ['POST'])
+def find_book():
+    title = request.form.get("title")
+    b = Book.query.filter(Book.title == title).first()
+    title = b.title
+    authorName = b.authors.all()[0].name
+    authorLastName = b.authors.all()[0].lastName
+    if len(Book.query.filter(Book.title == title).first().authors.all()) > 1:
+        authorName2 = b.authors.all()[1].name
+        authorLastName2 = b.authors.all()[1].lastName
+    else:
+        authorName2 = ""
+        authorLastName2 = ""
+    
+    return render_template('update.html', id = id, title = title, authorName = authorName, authorName2 = authorName2, authorlastName = authorLastName, authorLastName2 = authorLastName2, available = b.is_available)
+
+@app.route("/update", methods =['POST'])
+def update_book():
+    title = request.form.get("title")
+    authorName = request.form.get("authorName")
+    authorLastName = request.form.get("authorLastName")
+    authorName2 = request.form.get("authorName2")
+    authorLastName2 = request.form.get("authorLastName2")
+    available = request.form.get('available')
+    if available:
+        available = True
+    else:
+        available = False
+    
+    b = Book.query.filter(Book.title == title).first()
+    b.title = title
+    b.is_available = available
+    db.session.add(b)
+    b.authors = []
+    if not Author.query.filter(Author.name == authorName, Author.lastName == authorLastName).first():
+        a = Author(name = authorName, lastName = authorLastName)
+    else:
+        a = Author.query.filter(Author.name == authorName, Author.lastName == authorLastName).first()
+    b.authors.append(a)
+    db.session.add(a)
+    
+    if not authorName2 == "":
+        if not Author.query.filter(Author.name == authorName2, Author.lastName == authorLastName2).first():
+            a2 = Author(name = authorName2, lastName = authorLastName2)
+        else:
+            a2 = Author.query.filter(Author.name == authorName2, Author.lastName == authorLastName2).first()
+        b.authors.append(a2)
+        db.session.add(a2)
+    db.session.commit()
+    return redirect('/')
