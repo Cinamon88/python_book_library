06285a0ecee422d3b16b38c364669f7e815d7d3a
Lukasz Gorka <lukaszgorka@hotmail.com>
2023-03-22 11:30:20 +0100

    add route add_book

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
-from flask import render_template
+from flask import render_template, request, redirect
 from app import app
-from app.models import Book
+from app.models import Book, Author, db
 
 @app.route("/")
 def index():
@@ -10,3 +10,30 @@ def index():
 @app.route('/add', methods=['GET'])
 def add():
     return render_template("add.html")
+
+@app.route("/add", methods =['POST'])
+def add_book():
+    title = request.form.get("title")
+    authorName = request.form.get("authorName")
+    authorLastName = request.form.get("authorLastName")
+    authorName2 = request.form.get("authorName2")
+    authorLastName2 = request.form.get("authorLastName2")
+    b = Book(title = title)
+    db.session.add(b)
+
+    if not Author.query.filter(Author.name == authorName, Author.lastName == authorLastName).first():
+        a = Author(name = authorName, lastName = authorLastName)
+    else:
+        a = Author.query.filter(Author.name == authorName, Author.lastName == authorLastName).first()
+    b.authors.append(a)
+    db.session.add(a)
+
+    if not authorName2 == "":
+        if not Author.query.filter(Author.name == authorName2, Author.lastName == authorLastName2).first():
+            a2 = Author(name = authorName2, lastName = authorLastName2)
+        else:
+            a2 = Author.query.filter(Author.name == authorName2, Author.lastName == authorLastName2).first()
+        b.authors.append(a2)
+        db.session.add(a2)
+    db.session.commit()
+    return redirect('/')   
