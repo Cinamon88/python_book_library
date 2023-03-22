0dc86dbe5aee9d02dfa549bd3f93c7b15483fbbf
Lukasz Gorka <lukaszgorka@hotmail.com>
2023-03-22 11:05:51 +0100

    add route to add book

--- app/routes.py
+++ app/routes.py
@@ -6,3 +6,7 @@ from app.models import Book
 def index():
     all_books = Book.query.all()
     return render_template("homepage.html", all_books=all_books)
+
+@app.route('/add', methods=['GET'])
+def add():
+    return render_template("add.html")
